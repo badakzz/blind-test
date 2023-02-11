1eca85ae1cfa25c0c5f1919147f36bb09eb223be
badakzz <lucas.deray@gmail.com>
2023-02-12 00:22:29 +0100

    wip

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -4,13 +4,13 @@ import Link from 'next/link'
 import { User } from '../interfaces'
 
 type Props = {
-  data: User
+    data: User
 }
 
 const ListItem = ({ data }: Props) => (
-  <Link href="/users/[id]" as={`/users/${data.id}`}>
-    {data.id}:{data.name}
-  </Link>
+    <Link href="/users/[id]" as={`/users/${data.id}`}>
+        {data.id}:{data.name}
+    </Link>
 )
 
 export default ListItem
