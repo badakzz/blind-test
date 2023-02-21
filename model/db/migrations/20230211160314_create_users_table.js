376ba083f790ab7b8276815df9faa304c8a97937
badakzz <lucas.deray@gmail.com>
2023-02-21 16:27:14 +0100

    wip

---
+++ model/db/migrations/20230211160314_create_users_table.js
@@ -0,0 +1,14 @@
+exports.up = function (knex) {
+    return knex.schema.createTable("users", function (table) {
+        table.increments()
+        table.string("username").notNullable()
+        table.string("email").notNullable().unique()
+        table.string("password").notNullable()
+        table.boolean("is_active").notNullable().defaultTo(true)
+        table.timestamps(true, true)
+    })
+}
+
+exports.down = function (knex) {
+    return knex.schema.dropTable("users")
+}
